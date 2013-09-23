d1f1d9835474e2209c68b251da18f5e332a1edb0
Yash Shah <mail@yashshah.com>
2013-09-23 19:01:00 +0530

    Remove internal helpers as it is not supported in new devise version

--- lib/devise_sms_confirmable/routes.rb
+++ lib/devise_sms_confirmable/routes.rb
@@ -5,7 +5,7 @@ module ActionDispatch::Routing
     def devise_sms_confirmation(mapping, controllers)
       resource :sms_confirmation, :only => [:new, :create], :path => mapping.path_names[:sms_confirmation], :controller => controllers[:sms_confirmations]
       # if these routes created then module sms_confirmable is using so add special helpres for Devise::Controllers::InternalHelpers
-      Devise::Controllers::InternalHelpers.send(:include, DeviseSmsConfirmable::Controllers::InternalHelpers)
+      #Devise::Controllers::InternalHelpers.send(:include, DeviseSmsConfirmable::Controllers::InternalHelpers)
     end
 
   end
